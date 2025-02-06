6527e1b53125f858f1afd47ae9da4817e69b9ad3
EspoirYEGANGBEDE2005 <yegangbedeespoir@gmail.com>
2025-02-06 07:57:36 +0100

    nombrePairsImpairs.java

---
+++ nombrePairsImpairs.java
@@ -0,0 +1,31 @@
+package programme;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 System.out.print("Entrer un nombre svp:");
+		 
+	       Scanner n1 = new Scanner(System.in);
+	       int n2 = n1.nextInt();
+	       if (n2 % 2 == 0) {
+	    	   System.out.print("Le nombre est pair");
+	       } 
+	       else {
+	    	   System.out.print("Le nombre est impair");
+	       }
+	       
+	      
+
+			 
+
+		}
+
+	
+}
+	
+	
+		
+		
+		 
+		
